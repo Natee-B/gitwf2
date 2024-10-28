4ddfda2e621872ad94b5929247a9a87aeb5bd9a3
Natee-B <nateebunyalug1994@gmail.com>
2024-10-28 14:55:40 +0700

    first

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+app.use()
+
+const port = process.env.PORT || 8889
+app.listen(port,()=>console.log("Server Run 8888"))
